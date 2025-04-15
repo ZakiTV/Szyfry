e9f9fc2f9d760a44c2ada818a2c4cab80c628b59
Paweł Kopczyk <140538987+ZakiTV@users.noreply.github.com>
2025-04-16 01:23:40 +0200

    dodanie komentarzy do glownego programu

--- Szyfr/Program.cs
+++ Szyfr/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Szyfr;
-
-Console.WriteLine("Podaj litere do zaszyfrowania");
+//Wyslanie zapytania o haslo do zaszyfrowania
+Console.WriteLine("Podaj haslo do zaszyfrowania");
+//wyczytanie stringa z wprowadzonego hasla
 string text = Console.ReadLine();
+// wyslanie do klasy stringa aby zaszyfrowac wybrana funkcja w tym przypadku wybrany szyfr GADERYPOLUKI
 string output = Cypher.GADERYPOLUKICypher(text);
-
-Console.WriteLine("Zaszyfrowana litera " + output);
+//Wyslanie zaszyfrowanego hasla po odebraniu go z klasy
+Console.WriteLine("Zaszyfrowane haslo " + output);
